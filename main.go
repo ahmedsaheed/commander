e1a7c0085c55a357e49e1a5971f8e083e99d9dd3
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-01 22:15:01 +0100

    initial view

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/PullRequestInc/go-gpt3"
 	"log"
 	_ "os"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 )
 
 func getCommand(word string) {
@@ -18,7 +21,7 @@ func getCommand(word string) {
 	ctx := context.Background()
 	client := gpt3.NewClient(apiKey)
 
-	resp, err := client.CompletionWithEngine(ctx, "text-davinci-001", gpt3.CompletionRequest{
+	resp, err := client.CompletionWithEngine(ctx, "text-davinci-002", gpt3.CompletionRequest{
 		Prompt:           []string{word},
 		MaxTokens:        gpt3.IntPtr(100),
 		Temperature:      gpt3.Float32Ptr(0),
@@ -35,5 +38,69 @@ func getCommand(word string) {
 }
 
 func main() {
-	getCommand("install go using brew")
+	p := tea.NewProgram(initialModel())
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type tickMsg struct{}
+type errMsg error
+
+type model struct {
+	textInput textinput.Model
+	err       error
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "I'm at yor service, what can I do for you?"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 50
+	ti.Prompt = "🐙 "
+
+	return model{
+		textInput: ti,
+		err:       nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		}
+
+		if msg.Type == tea.KeyEnter {
+			getCommand(m.textInput.View())
+			m.textInput.Update(textinput.New())
+
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"Commander\n\n%s\n\n%s",
+		m.textInput.View(),
+		"enter confirm • esc quit",
+	) + "\n"
 }

b1ae7d9f6a710cf415e30178eef5adc32549fd6e
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-02 01:35:14 +0100

    passed arguement for call

--- main.go
+++ main.go
@@ -4,67 +4,35 @@ import (
 	"context"
 	"fmt"
 	"github.com/PullRequestInc/go-gpt3"
-	"log"
-	"os"
-	"strings"
-
+	"github.com/charmbracelet/bubbles/progress"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"log"
 )
-
-var (
-	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
-	cursorStyle         = focusedStyle.Copy()
-	noStyle             = lipgloss.NewStyle()
-	helpStyle           = blurredStyle.Copy()
-	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
-
-	focusedButton = focusedStyle.Copy().Render("[ Submit ]")
-	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
-)
-
-type model struct {
-	focusIndex int
-	inputs     []textinput.Model
-	cursorMode textinput.CursorMode
-}
 
 type response string
 type errMsg struct{ err error }
+type model struct {
+	textInput textinput.Model
+	progress  progress.Model
+	response  string
+	err       error
+}
 
 func initialModel() model {
-	m := model{
-		inputs: make([]textinput.Model, 3),
+	ti := textinput.New()
+	ti.Placeholder = "I'm at yor service, what can I do for you?"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 50
+	ti.Prompt = "🐙 "
+
+	return model{
+		textInput: ti,
+		err:       nil,
 	}
-
-	var t textinput.Model
-	for i := range m.inputs {
-		t = textinput.New()
-		t.CursorStyle = cursorStyle
-		t.CharLimit = 32
-
-		switch i {
-		case 0:
-			t.Placeholder = "Nickname"
-			t.Focus()
-			t.PromptStyle = focusedStyle
-			t.TextStyle = focusedStyle
-		case 1:
-			t.Placeholder = "Email"
-			t.CharLimit = 64
-		case 2:
-			t.Placeholder = "Password"
-			t.EchoMode = textinput.EchoPassword
-			t.EchoCharacter = '•'
-		}
-
-		m.inputs[i] = t
-	}
-
-	return m
 }
+
 func (m model) Init() tea.Cmd {
 	return textinput.Blink
 }
@@ -92,118 +60,63 @@ func getCommand(word string) string {
 		log.Fatalln(err)
 	}
 	//fmt.Println(resp)
-	fmt.Println(resp.Choices[0].Text)
 	return resp.Choices[0].Text
 
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
 
-		// Change cursor mode
-		case "ctrl+r":
-			m.cursorMode++
-			if m.cursorMode > textinput.CursorHide {
-				m.cursorMode = textinput.CursorBlink
-			}
-			cmds := make([]tea.Cmd, len(m.inputs))
-			for i := range m.inputs {
-				cmds[i] = m.inputs[i].SetCursorMode(m.cursorMode)
-			}
-			return m, tea.Batch(cmds...)
-
-		// Set focus to next input
-		case "tab", "shift+tab", "enter", "up", "down":
-			s := msg.String()
-
-			// Did the user press enter while the submit button was focused?
-			// If so, exit.
-			if s == "enter" && m.focusIndex == len(m.inputs) {
-				println(getCommand(m.inputs[0].Value()))
-				//println("Submitting form \n Nickname:", m.inputs[0].Value(), "\n Email:", m.inputs[1].Value())
+		case "enter":
+			if m.textInput.Value() != "" {
+				m.response = getCommand(m.textInput.Value())
+				println(getCommand(m.textInput.Value()))
+				println()
 				return m, tea.Quit
-			}
-
-			// Cycle indexes
-			if s == "up" || s == "shift+tab" {
-				m.focusIndex--
 			} else {
-				m.focusIndex++
+				cmd = textinput.Blink
 			}
 
-			if m.focusIndex > len(m.inputs) {
-				m.focusIndex = 0
-			} else if m.focusIndex < 0 {
-				m.focusIndex = len(m.inputs)
-			}
-
-			cmds := make([]tea.Cmd, len(m.inputs))
-			for i := 0; i <= len(m.inputs)-1; i++ {
-				if i == m.focusIndex {
-					// Set focused state
-					cmds[i] = m.inputs[i].Focus()
-					m.inputs[i].PromptStyle = focusedStyle
-					m.inputs[i].TextStyle = focusedStyle
-					continue
-				}
-				// Remove focused state
-				m.inputs[i].Blur()
-				m.inputs[i].PromptStyle = noStyle
-				m.inputs[i].TextStyle = noStyle
-			}
-
-			return m, tea.Batch(cmds...)
 		}
+
+		//case response:
+		//	m.response = string(msg)
+		//	return m, tea.Quit
+		//
+		//// We handle errors just like any other message
+		//case errMsg:
+		//	m.err = msg.err
+		//	return m, tea.Quit
 	}
 
-	// Handle character input and blinking
-	cmd := m.updateInputs(msg)
-
+	m.textInput, cmd = m.textInput.Update(msg)
 	return m, cmd
 }
 
-func (m *model) updateInputs(msg tea.Msg) tea.Cmd {
-	var cmds = make([]tea.Cmd, len(m.inputs))
-
-	// Only text inputs with Focus() set will respond, so it's safe to simply
-	// update all of them here without any further logic.
-	for i := range m.inputs {
-		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
-	}
-
-	return tea.Batch(cmds...)
-}
-
 func (m model) View() string {
-	var b strings.Builder
 
-	for i := range m.inputs {
-		b.WriteString(m.inputs[i].View())
-		if i < len(m.inputs)-1 {
-			b.WriteRune('\n')
-		}
+	if m.err != nil {
+		return fmt.Sprintf("Error: %s", m.err)
 	}
-
-	button := &blurredButton
-	if m.focusIndex == len(m.inputs) {
-		button = &focusedButton
-	}
-	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
-
-	b.WriteString(helpStyle.Render("cursor mode is "))
-	b.WriteString(cursorModeHelpStyle.Render(m.cursorMode.String()))
-	b.WriteString(helpStyle.Render(" (ctrl+r to change style)"))
-
-	return b.String()
+	return fmt.Sprintf(
+		"Commander\n\n%s\n\n%s",
+		m.textInput.View(),
+		"enter confirm • esc quit",
+	) + "\n"
 }
 
+////_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
 func main() {
-	if err := tea.NewProgram(initialModel()).Start(); err != nil {
-		fmt.Printf("could not start program: %s\n", err)
-		os.Exit(1)
+	p := tea.NewProgram(initialModel())
+
+	if err := p.Start(); err != nil {
+		log.Fatal(err)
 	}
 }
